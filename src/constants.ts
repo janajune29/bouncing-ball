7fa59567731f2b74642a9349906f2abc0a3a67e1
janajune29 <jnickason@kameleoon.com>
2024-05-21 14:21:04 -0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "lua2fwcok3";
+export const siteCode = lua2fwcok3;
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
